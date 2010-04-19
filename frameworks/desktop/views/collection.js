75b6bf04bf419cfc1431000fd7765076e41e5c0f
Christopher Swasey <christopher.swasey@gmail.com>
2010-04-19 05:49:26 -0400

    initial render now works

--- frameworks/desktop/views/collection.js
+++ frameworks/desktop/views/collection.js
@@ -907,21 +907,31 @@ SC.CollectionView = SC.View.extend(
 
 
   addItemViewForRowAndColumn: function(row, column, rebuild) {
+	console.log("adding", row, column)
     var view, itemViews, layer, existing, element, rowView
       del  = this.get('contentDelegate')
 
     itemViews  = this._sc_itemViews;
+		
+		// debugger
+
 		if(!itemViews)
-			itemViews = []
+			this._sc_itemViews = itemViews = []
+		
+		// debugger
 		
 		if(!itemViews[row])
 			itemViews[row] = []
-			
+
+		// debugger
+					
     existing = itemViews[row][column];
     containerView = this.get('containerView') || this
 
 		if(!SC.none(column)) {
 			rowView = itemViews[row][-1]
+			
+			// debugger
 			
 			if(!rowView || !rowView.get)  {
 				rowView = this.addItemViewForRowAndColumn(row, null, rebuild)
@@ -975,6 +985,7 @@ SC.CollectionView = SC.View.extend(
       view = element
     } else {
       itemViews[row][column] = view
+			// debugger;
     }
     
     containerView.appendChild(view)
@@ -1134,10 +1145,10 @@ SC.CollectionView = SC.View.extend(
     else
       attrs.classNames = this._COLLECTION_CLASS_NAMES;
 
-		if(column) {
-			attrs.classNames.push('cell')
-			attrs.classNames.push('column-' + column)
-		}
+		// if(column) {
+			// attrs.classNames.push('cell')
+			// attrs.classNames.push('column-' + column)
+		// }
 
     return attrs
   },
