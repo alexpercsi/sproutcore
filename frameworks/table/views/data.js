74330027f01bb7b3b3d2f9cba7c06b9597cb31c0
Christopher Swasey <christopher.swasey@gmail.com>
2010-04-19 06:32:18 -0400

    remove more vestigial, commented out methods from dataview now that it works without them

--- frameworks/table/views/data.js
+++ frameworks/table/views/data.js
@@ -20,10 +20,6 @@ SC.DataView = SC.ListView.extend({
 		return this.get('dataSource')
 	}.property('dataSource').cacheable(),
 	
-	// collectionViewWillDisplayCellForRowAndColumn: function(collectionView, view, row, column) {
-		// this is where we need to insert the actual content for the cell
-	// },
-
 	row: function() {
 		var rows = this.get('rows'),
 			hiddenRows = this.get('hiddenRows'),
@@ -150,40 +146,6 @@ SC.DataView = SC.ListView.extend({
 		hiddenRows.push(view)
 		delete itemViews[row]
   },
-  // 
-  // reloadSelectionIndexesIfNeeded: function() {
-  //   var invalid = this._invalidSelection, view
-  //   if (!invalid || !this.get('isVisibleInWindow')) return this 
-  //   this._invalidSelection = NO
-  // 		invalid.forEach(function(index) {
-  // 			view = this.viewForRow(index)
-  // 			SC.$(view).setClass("sel", this.isSelected(index))
-  // 		}, this)
-  // },
-  
-  // contentIndexForLayerId: function(id) {
-  // 		var rowsHash = this.get('rowsHash')
-  //   if (!id || !(id = id.toString())) return null ; // nothing to do
-  //   
-  //   var base = this._baseLayerId;
-  //   if (!base) base = this._baseLayerId = SC.guidFor(this)+"-";
-  //   
-  //   // no match
-  //   if ((id.length <= base.length) || (id.indexOf(base) !== 0)) return null ; 
-  // 
-  //   var ret = Number(id.split('-').objectAt(1))
-  //   return isNaN(ret) ? null : rowsHash[ret] ;
-  // },
-  
-	//   contentIndexForCell: function(cell) {
-	// 	var rowsHash = this.get('rowsHash')
-	// 	var row = this.get('rows').indexOf(cell.parentNode)
-	// 	return rowsHash[row]
-	//   },
-	// 
-	// selectionIndexForItemView: function(cell) {
-	// 	return this.selectionIndexForCell(cell)
-	// },
 
   selectionIndexForCell: function(cell) {
 		var rowsHash = this.get('rowsHash')
@@ -199,45 +161,10 @@ SC.DataView = SC.ListView.extend({
 		return index * columns.get('length') + column
   },
 
-  // scrollToItemView: function(view) {},
-
   isSelected: function(item) {
     var sel = this.get('selection')
     return sel ? sel.contains(this.get('content'), item) : NO
   },
-
-  // columnsInRect: function(rect) {
-  //   var columns = this.get('columns'),
-  //       left       = SC.minY(rect),
-  //       right    = SC.maxY(rect),
-  //       width    = rect.height || 0,
-  //       len, offset, start, end;
-  // 
-  // 		if(SC.none(columns))
-  // 			return null
-  // 			
-  // 	start = -1
-  // 		while(this.offsetForColumn(start + 1) < left) {
-  // 			start++
-  // 		}
-  // 
-  // 		end = start
-  // 		do {
-  // 			end++
-  // 		} while(this.offsetForColumn(end) < right) 
-  // 		
-  // 		
-  // 		//     end = start + ((height - (height % rowHeight)) / rowHeight) ;
-  // 		//     // if (end > len) end = len;
-  // 		//     offset = this.rowOffsetForContentIndex(end);
-  // 		// 
-  // 		// end = len
-  // 		// while(this.offsetForColumn(end) > right) {
-  // 		// 	end--
-  // 		// }
-  // 		
-  //   return SC.IndexSet.create(start, end-start);
-  // },
 
   nowShowingColumns: function() {
     return this.computeNowShowingColumns();
@@ -246,72 +173,6 @@ SC.DataView = SC.ListView.extend({
 	allColumns: function() {
    return SC.IndexSet.create(0, this.getPath('columns.length')).freeze();
   }.property('columns').cacheable(),
-
-
-	// 
-	//   computeNowShowingColumns: function() {
-	//     var r = this.columnsInRect(this.get('clippingFrame'));
-	//     if (!r) r = this.get('allColumns'); // default show all
-	// 
-	//     // make sure the index set doesn't contain any indexes greater than the
-	//     // actual content.
-	//     else {
-	//       var len = this.getPath('columns.length'), 
-	//           max = r.get('max');
-	//       if (max > len) r = r.copy().remove(len, max-len).freeze();
-	//     }
-	// 
-	//     return r ;
-	//   },
-	// 
-	// 
-	//   _cv_nowShowingColumnsDidChange: function() {
-	//     var nowShowing  = this.get('nowShowingColumns'),
-	//         last        = this._sccv_lastNowShowingColumns,
-	//         diff, diff1, diff2;
-	// 
-	//     diff1 = this._TMP_DIFF5
-	//     diff2 = this._TMP_DIFF6
-	// 
-	//     if (diff1) diff1.clear();
-	//     if (diff2) diff2.clear();
-	// 
-	//     // find the differences between the two
-	//     // NOTE: reuse a TMP IndexSet object to avoid creating lots of objects
-	//     // during scrolling
-	//     if (last !== nowShowing) {
-	//       if (last && nowShowing) {
-	//         diff1 = this._TMP_DIFF1.add(last).remove(nowShowing);
-	//         diff2 = this._TMP_DIFF2.add(nowShowing).remove(last);
-	//         diff = diff1.add(diff2);
-	//       } else diff = last || nowShowing ;
-	//     }
-	// 
-	//     // if nowShowing has actually changed, then update
-	//     if (diff && diff.get('length') > 0) {
-	//       this._sccv_lastNowShowing = nowShowing ? nowShowing.frozenCopy() : null;
-	//       this.reload(null, diff);
-	//     }
-	//   }.observes('nowShowingColumns'),
-	// 
-	// offsetForColumn: function(column) {
-	// 	var offsets = this._columnOffests,
-	// 		offset = offsets.objectAt(column)
-	// 	
-	// 	if(!offset)
-	// 		offset = column > 0 ? this.offsetForColumn(column - 1) + this.widthForColumn(column - 1) : 0
-	// 		
-	// 	offsets.replace(column, 1, offset)
-	// 	
-	// 	return offset
-	// },
-	// 
-	// widthForColumn: function(column) {
-	// 	var columns = this.get('columns'),
-	// 		column = columns.objectAt(column)
-	// 	
-	// 	return column ? column.get('width') || 120 : 120
-	// }
 
   computeLayout: function() {
     var ret = this._sclv_layout;
